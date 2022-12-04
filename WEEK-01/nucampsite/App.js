2591451db49d86137b7bbd88afc969bc415e9139
Marti D <marti.dolce@29signals.org>
2022-12-04 07:35:10 -0500

    week-01_Set up images and campsites data
    Install React Native Elements
    Create MainComponent.js and DirectoryScreen.js
    Update Apps.js

--- WEEK-01/nucampsite/App.js
+++ WEEK-01/nucampsite/App.js
@@ -1,25 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './screens/MainComponent';
+
 
 export default function App() {
-  return (
-
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app on IOS and Android</Text>
-      <Text>@nd text</Text>
-
-      <StatusBar style="auto" />
-    </View>
-
-
-  );
+    return <Main />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
--- WEEK-01/nucampsite/screens/MainComponent.js
+++ WEEK-01/nucampsite/screens/MainComponent.js
@@ -1,5 +1,15 @@
-import {useState} from 'react';
-import CAMPSITES from '../shared/'
-import {DirectoryScreen} from './DirectoryScreen'
+import { useState } from 'react';
+import { CAMPSITES } from '../shared/campsites';
+import DirectoryScreen from './DirectoryScreen';
 
 // Create a constant named Main set equal to an arrow function and export Main at the bottom of this file outside of the arrow function body.
+const Main = () => {
+    const [campsites, setCampsites] = useState(CAMPSITES);
+
+    /**
+     * Return the DirectoryScreen tag from the return statement passing a single prop named campsites equal to the campsites state variable.
+      */
+    return <DirectoryScreen campsites={campsites} />;
+
+};
+export default Main;
