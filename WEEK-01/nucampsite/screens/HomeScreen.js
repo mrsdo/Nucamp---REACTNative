6866dd8aa7c0387078b55f8c1c8fb894181dab1c
Marti D <marti.dolce@29signals.org>
2022-12-06 06:18:02 -0500

    week-01_Update HomeScreen with campsite, promotions and partner data
    Added FeaturedItem Component as JSX with prop item to appear on Homescreen

--- WEEK-01/nucampsite/screens/HomeScreen.js
+++ WEEK-01/nucampsite/screens/HomeScreen.js
@@ -1,18 +1,68 @@
 /**
  * HomeScreen
  */
-
 import React from'react';
+import { useState } from 'react';
 import {
-  View,
-  Text
+    ScrollView,
+    View,
+    Text
 } from'react-native';
+import { Card } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
+import { PROMOTIONS } from '../shared/promotions';
+import { PARTNERS } from '../shared/partners';
+
+// Add Featured items Component
+const FeaturedItem = ({item}) => {
+    if (item) {
+        return (
+            <Card containerStyle={{padding: 0}}>
+                <Card.Image source={item.image} >
+                    <View style={{justifyContent: 'center', flex: 1}}>
+                        <Text
+                            style={{
+                                color: 'white',
+                                textAlign: 'center',
+                                fontSize: 20
+                        }}>
+                            {item.name}
+
+                        </Text>
+                    </View>
+                </Card.Image>
+                <Text
+                    style={{
+                        margin: 20
+                }}>
+                    {item.description}
+
+                </Text>
+            </Card>
+        )
+    }
+    return <View />;
+}
 
 const HomeScreen = () =>{
+    // Create state variables for:
+    const [campsites, setCampsites] = useState(CAMPSITES);
+    const [promotions, setPromotions] = useState(PROMOTIONS);
+    const [partners, setPartners] = useState(PARTNERS);
+
+    // Add objects to hold featured items for campsites, partners and promotions
+    const featCampsite = campsites.find((item) => item.featured);
+    const featPromotion = promotions.find((item) => item.featured);
+    const featPartner = partners.find((item) => item.featured);
+
+
     return (
-        <View style={{flex:1}}>
-            <Text>Home Screen</Text>
-        </View>
+        //ScrollView loads all child components at once v FlatList is a better choice if list is longer
+        <ScrollView>
+            <FeaturedItem item={featCampsite} />
+            <FeaturedItem item={featPromotion} />
+            <FeaturedItem item={featPartner} />
+        </ScrollView>
     );
 
 };
