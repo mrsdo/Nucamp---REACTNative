4e8bd6f583084cdff875ac1844a9cb1c5e884be8
Marti D <marti.dolce@29signals.org>
2022-12-14 13:01:20 -0600

    Add Provider and store to enable redux
    Set up your React Native app to connect the components to Redux.
    
    Obtain state information from Redux.
    
    Dispatch actions to the Redux store to update the state.

--- nucampsite/App.js
+++ nucampsite/App.js
@@ -1,10 +1,14 @@
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import Main from './screens/MainComponent';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Main />
+            <Provider store={store}>
+                <Main />
+            </Provider>
         </NavigationContainer>
     );
 }
