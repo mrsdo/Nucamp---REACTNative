23991fa4455426f978268a42543b10ee7400b1cd
Marti D <marti.dolce@29signals.org>
2022-12-20 16:30:35 -0500

    Use animation to give feedback to users gestures

--- nucampsite/features/campsites/RenderCampsite.js
+++ nucampsite/features/campsites/RenderCampsite.js
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import {PanResponder,  Alert,  StyleSheet, Text, View} from 'react-native';
 import {Card, Icon} from 'react-native-elements';
 import {baseUrl} from '../../shared/baseUrl';
@@ -7,14 +8,34 @@ const RenderCampsite = (props) => {
 
     const {campsite} = props;
 
-    // add a constant called isLeftSwipe equal to an arrow function that destructures dx in the parameter list and returns the boolean expression dx < -200.
+/*  Add a constant called isLeftSwipe equal to an arrow function that
+*   destructures dx in the parameter list and returns the boolean expression dx < -200.
+*/
+
+    // create a constant called view equal to useRef().
+    const view = useRef();
+
     const isLeftSwipe = ({ dx }) => dx < -200;
 
-    // create a constant called panResponder equal to a call to PanResponder.create() passing an object as the argument with the following properties:
-    // onStartShouldSetPanResponder equal to an arrow function that returns true.
-    // onPanResponderEnd equal to an arrow function with two parameters of e as the first and gestureState as the second.
+/*  create a constant called panResponder equal to a call to PanResponder.create() passing an object as the
+*   argument with the following properties:
+*   onStartShouldSetPanResponder equal to an arrow function that returns true.
+*   onPanResponderEnd equal to an arrow function with two parameters of e as the first and gestureState as the second.
+*/
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
+
+/*  Add a new panHandler inside the PanResponder.create object called onPanResponderGrant equal to an arrow function.
+*   You can put this right after the onStartShouldSetPanResponder line.
+*/
+        onPanResponderGrant: () => {
+            view.current
+                .rubberBand(1000)
+                .then((endState) =>
+                    console.log(endState.finished ? 'finished' : 'canceled')
+                );
+        },
+
         onPanResponderEnd: (e, gestureState) => {
             console.log('pan responder end', gestureState);
             if (isLeftSwipe(gestureState)) {
@@ -52,6 +73,7 @@ const RenderCampsite = (props) => {
                 animation='fadeInDownBig'
                 duration={2000}
                 delay={1000}
+                ref={view}
                 {...panResponder.panHandlers}
             >
             <Card containerStyle={styles.cardContainer}>
